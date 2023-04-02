e56162d898269a1a3ec7f268ad9e4dae947ddd20
Joalor64 <rhgarcon@gmail.com>
2023-04-02 16:07:48 -0400

    got some stuff in

--- source/Number.hx
+++ source/Number.hx
@@ -2,29 +2,8 @@ package;
 
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
-import flixel.group.FlxTypedSpriteGroup;
 
-using flixel.util.FlxSpriteUtil;
-
-class Number extends FlxSprite
+class Ornament extends FlxSprite
 {
-	var number:FlxTypedSpriteGroup<FlxSprite>;
-
-	public function new()
-	{
-		public static inline final NUMBERS:Array<String> = [
-            	// placeholders from a dead project
-			"yellow", "aqua", "blue", "cyan", "gold", "green", "indigo", "lime", "magenta", "mint", "orange", "pink",
-			"purple", "red", "rose", "scarlet", "teal", "white"
-		];
-
-        		loadGraphic(Paths.image('numbers/$NUMBERS'));
-
-		number = new FlxTypedSpriteGroup<FlxSprite>();
-
-		if (number != null)
-		{
-			number.add(this);
-		}
-	}
+    // work in progress
 }
