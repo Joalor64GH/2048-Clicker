eed6ea171628e3b1724db2b1b874b1fa216f7955
Joalor64 <rhgarcon@gmail.com>
2023-04-03 12:54:12 -0400

    Update Number.hx

--- source/Number.hx
+++ source/Number.hx
@@ -1,6 +1,5 @@
 package;
 
-import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
 
@@ -8,41 +7,24 @@ import flixel.graphics.frames.FlxAtlasFrames;
 
 class Number extends FlxSprite
 {
-    /*public static final NUMBERS:Array<String> = [
-	'1', 
-        '2', 
-        '4',
-        '8',  
-        '16', 
-        '32', 
-        '64', 
-        '128', 
-        '256', 
-        '512', 
-        '1024', 
-        '2048',
-	'4096', 
-        '8192', 
-        '16384', 
-        '32768', 
-        '65536', 
-        '131072', 
-        '262144', 
-        '524288', 
-        '1048576'
-    ];*/
-
     public function new()
     {
         super();
-        // loadGraphic(Paths.image('numbers/$NUMBERS'));
     }
 }
 
 class Exponent extends Number
 {
+    public function new()
+    {
+        super();
+    }
 }
 
 class Infinity extends Number
 {
+    public function new()
+    {
+        super();
+    }
 }
