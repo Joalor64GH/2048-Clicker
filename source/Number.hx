e3ce15b1c677820f213aa82f657fe3a932d00dd0
Joalor64 <rhgarcon@gmail.com>
2023-04-06 10:02:58 -0400

    Update Number.hx

--- source/Number.hx
+++ source/Number.hx
@@ -5,9 +5,9 @@ import flixel.graphics.frames.FlxAtlasFrames;
 
 class Number extends FlxSprite
 {
-    	public var num:FlxSprite;
+    public var num:FlxSprite;
 	
-	public static final NUMBERS:Array<String> = [
+    public static final NUMBERS:Array<String> = [
 	'1', 
         '2', 
         '4',
@@ -31,20 +31,20 @@ class Number extends FlxSprite
         '1048576'
     ];
 
-	public function new(x:Float, y:Float)
-	{
-		super(x, y);
+    public function new(x:Float, y:Float)
+    {
+	super(x, y);
 
-		num = new FlxSprite().loadGraphic(Paths.image('numbers/default/' + $NUMBERS));
-		add(num);
-	}
+	num = new FlxSprite().loadGraphic(Paths.image('numbers/default/' + $NUMBERS));
+	add(num);
+    }
 }
 
 class Exponent extends FlxSprite
 {
-    	public var expo:FlxSprite;
+    public var expo:FlxSprite;
 
-	public static final EXPONENTS:Array<String> = [
+    public static final EXPONENTS:Array<String> = [
 	'21', 
         '22', 
         '23',
@@ -64,37 +64,37 @@ class Exponent extends FlxSprite
         '100'
     ];
 
-	public function new(x:Float, y:Float)
-	{
-		super(x, y);
+    public function new(x:Float, y:Float)
+    {
+	super(x, y);
 
-		expo = new FlxSprite().loadGraphic(Paths.image('numbers/exponents/' + $EXPONENTS));
-		add(expo);
-	}
+	expo = new FlxSprite().loadGraphic(Paths.image('numbers/exponents/' + $EXPONENTS));
+	add(expo);
+    }
 }
 
 // work in progress bc i have no idea how atlas works
 class Infinity extends FlxSprite
 {
-	public function new(x:Float, y:Float)
-	{
-		super(x, y);
+    public function new(x:Float, y:Float)
+    {
+	super(x, y);
 
-		var tex:FlxAtlasFrames;
+	var tex:FlxAtlasFrames;
 
-		tex = Paths.getSparrowAtlas('numbers/infinity');
-		frames = tex;
+	tex = Paths.getSparrowAtlas('numbers/infinity');
+	frames = tex;
 
-		// using code from fnf as placeholders
-		/*animation.addByPrefix('bf', "BF idle dance white", 24);
-		animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
-		animation.addByPrefix('gf', "GF Dancing Beat WHITE", 24);
-		animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
-		animation.addByPrefix('spooky', "spooky dance idle BLACK LINES", 24);
-		animation.addByPrefix('pico', "Pico Idle Dance", 24);
-		animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
-		animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
-		animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
-		animation.addByPrefix('tankman', "Tankman Menu BLACK", 24);*/
-	}
+	// using code from fnf as placeholders
+	/*animation.addByPrefix('bf', "BF idle dance white", 24);
+	animation.addByPrefix('bfConfirm', 'BF HEY!!', 24, false);
+	animation.addByPrefix('gf', "GF Dancing Beat WHITE", 24);
+	animation.addByPrefix('dad', "Dad idle dance BLACK LINE", 24);
+	animation.addByPrefix('spooky', "spooky dance idle BLACK LINES", 24);
+	animation.addByPrefix('pico', "Pico Idle Dance", 24);
+	animation.addByPrefix('mom', "Mom Idle BLACK LINES", 24);
+	animation.addByPrefix('parents-christmas', "Parent Christmas Idle", 24);
+	animation.addByPrefix('senpai', "SENPAI idle Black Lines", 24);
+	animation.addByPrefix('tankman', "Tankman Menu BLACK", 24);*/
+    }
 }
