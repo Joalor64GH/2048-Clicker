347a75d9cfc733b1231cbb88830d661fdf1d54b3
Joalor64 <rhgarcon@gmail.com>
2023-04-06 19:41:13 -0400

    my eyes hurt now

--- source/Number.hx
+++ source/Number.hx
@@ -6,63 +6,151 @@ import flixel.graphics.frames.FlxAtlasFrames;
 
 class Number extends FlxSprite
 {
-    public static final NUMBERS:Array<String> = [
-	'1', 
-        '2', 
-        '4',
-        '8',  
-        '16', 
-        '32', 
-        '64', 
-        '128', 
-        '256', 
-        '512', 
-        '1024', 
-        '2048',
-	'4096', 
-        '8192', 
-        '16384', 
-        '32768', 
-        '65536', 
-        '131072', 
-        '262144', 
-        '524288', 
-        '1048576'
-    ];
+    public var num:String = 'one';
 
     public function new(x:Float, y:Float)
     {
 	super(x, y);
-	loadGraphic(Paths.image('numbers/default/$NUMBERS'));
+	
+	switch(num)
+	{
+	    case "one":
+	        loadGraphic(Paths.image('numbers/default/1'));
+			
+	    case "two":
+	        loadGraphic(Paths.image('numbers/default/2'));
+			
+	    case "four":
+	        loadGraphic(Paths.image('numbers/default/4'));
+	
+	    case "eight":
+	        loadGraphic(Paths.image('numbers/default/8'));
+			
+	    case "sixteen":
+	        loadGraphic(Paths.image('numbers/default/16'));
+			
+	    case "thirtytwo":
+	        loadGraphic(Paths.image('numbers/default/32'));
+			
+	    case "sixtyfour":
+	        loadGraphic(Paths.image('numbers/default/64'));
+	
+	    case "onehundredtwentyeight":
+	        loadGraphic(Paths.image('numbers/default/128'));
+			
+	    case "twohundredfiftysix":
+	        loadGraphic(Paths.image('numbers/default/256'));
+			
+	    case "fivehundredtwelve":
+	        loadGraphic(Paths.image('numbers/default/512'));
+			
+	    case "thousandtwentyfour":
+	        loadGraphic(Paths.image('numbers/default/1024'));
+	
+	    case "twothousandfourtyeight":
+	        loadGraphic(Paths.image('numbers/default/2048'));
+			
+	    case "fourthousandninetysix":
+	        loadGraphic(Paths.image('numbers/default/4096'));
+			
+	    case "eightthousandonehundredninetytwo":
+	        loadGraphic(Paths.image('numbers/default/8192'));
+			
+	    case "sixteenthousandthreehundredeightyfour":
+	        loadGraphic(Paths.image('numbers/default/16384'));
+	
+	    case "thirtytwothousandsevenhundredsixtyeight":
+	        loadGraphic(Paths.image('numbers/default/32768'));
+			
+	    case "sixtyfivethousandfivehundredthirtysix":
+	        loadGraphic(Paths.image('numbers/default/65536'));
+			
+	    case "onehundredthirtyonethousandseventytwo":
+	        loadGraphic(Paths.image('numbers/default/131072'));
+			
+	    case "twohundredsixtytwothousandonehundredfourtyfour":
+	        loadGraphic(Paths.image('numbers/default/262144'));
+	
+	    case "fivehundredtwentyfourthousandtwohundredeightyeight":
+	        loadGraphic(Paths.image('numbers/default/524288'));
+			
+	    case "onemillionfourtyeighthousandfivehundredseventysix":
+	        loadGraphic(Paths.image('numbers/default/1048576'));
+	}
+    }
+			
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
     }
 }
 
 class Exponent extends FlxSprite
 {
-    public static final EXPONENTS:Array<String> = [
-	'21', 
-        '22', 
-        '23',
-        '24',  
-        '25', 
-        '26', 
-        '27', 
-        '28', 
-        '29', 
-        '30', 
-        '40', 
-        '50',
-	'60', 
-        '70', 
-        '80', 
-        '90', 
-        '100'
-    ];
+    public var expo:String = 'twentyone';
 
     public function new(x:Float, y:Float)
     {
 	super(x, y);
-	loadGraphic(Paths.image('numbers/exponents/$EXPONENTS'));
+	    
+	switch(num)
+	{
+	    case "twentyone":
+	        loadGraphic(Paths.image('numbers/exponents/21'));
+			
+	    case "twentytwo":
+	        loadGraphic(Paths.image('numbers/exponents/22'));
+			
+	    case "twentythree":
+	        loadGraphic(Paths.image('numbers/exponents/23'));
+	
+	    case "twentyfour":
+	        loadGraphic(Paths.image('numbers/exponents/24'));
+			
+	    case "twentyfive":
+	        loadGraphic(Paths.image('numbers/exponents/25'));
+			
+	    case "twentysix":
+	        loadGraphic(Paths.image('numbers/exponents/26'));
+			
+	    case "twentyseven":
+	        loadGraphic(Paths.image('numbers/exponents/27'));
+			
+	    case "twentyeight":
+	        loadGraphic(Paths.image('numbers/exponents/28'));
+	
+	    case "twentynine":
+	        loadGraphic(Paths.image('numbers/exponents/29'));
+			
+	    case "thirty":
+	        loadGraphic(Paths.image('numbers/exponents/30'));
+			
+	    case "fourty":
+	        loadGraphic(Paths.image('numbers/exponents/40'));
+			
+	    case "fifty":
+	        loadGraphic(Paths.image('numbers/exponents/50'));
+			
+	    case "sixty":
+	        loadGraphic(Paths.image('numbers/exponents/60'));
+			
+	    case "seventy":
+	        loadGraphic(Paths.image('numbers/exponents/70'));
+			
+	    case "eighty":
+	        loadGraphic(Paths.image('numbers/exponents/80'));
+			
+	    case "ninety":
+	        loadGraphic(Paths.image('numbers/exponents/90'));
+			
+	    case "onehundred":
+	        loadGraphic(Paths.image('numbers/exponents/100'));
+	}
+    }
+			
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
     }
 }
 
