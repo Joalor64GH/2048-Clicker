2adeaa2f2d8634abaa56362ccedd1743fd7bdecd
Joalor64 <rhgarcon@gmail.com>
2023-04-07 22:57:55 -0400

    hhuuuhh

--- source/Number.hx
+++ source/Number.hx
@@ -2,7 +2,6 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.graphics.frames.FlxAtlasFrames;
 
 class Number extends FlxSprite
 {
@@ -13,68 +12,68 @@ class Number extends FlxSprite
 	
 	switch(num)
 	{
-	    case "one":
-	        loadGraphic(Paths.image("numbers/default/1"));
+	    case 'one':
+	        loadGraphic(Paths.image('numbers/default/1'));
 			
-	    case "two":
-	        loadGraphic(Paths.image("numbers/default/2"));
+	    case 'two':
+	        loadGraphic(Paths.image('numbers/default/2'));
 			
-	    case "four":
-	        loadGraphic(Paths.image("numbers/default/4"));
+	    case 'four':
+	        loadGraphic(Paths.image('numbers/default/4'));
 	
-	    case "eight":
-	        loadGraphic(Paths.image("numbers/default/8"));
+	    case 'eight':
+	        loadGraphic(Paths.image('numbers/default/8'));
 			
-	    case "sixteen":
-	        loadGraphic(Paths.image("numbers/default/16"));
+	    case 'sixteen':
+	        loadGraphic(Paths.image('numbers/default/16'));
 			
-	    case "thirtytwo":
-	        loadGraphic(Paths.image("numbers/default/32"));
+	    case 'thirtytwo':
+	        loadGraphic(Paths.image('numbers/default/32'));
 			
-	    case "sixtyfour":
-	        loadGraphic(Paths.image("numbers/default/64"));
+	    case 'sixtyfour':
+	        loadGraphic(Paths.image('numbers/default/64'));
 	
-	    case "onehundredtwentyeight":
-	        loadGraphic(Paths.image("numbers/default/128"));
+	    case 'onehundredtwentyeight':
+	        loadGraphic(Paths.image('numbers/default/128'));
 			
-	    case "twohundredfiftysix":
-	        loadGraphic(Paths.image("numbers/default/256"));
+	    case 'twohundredfiftysix':
+	        loadGraphic(Paths.image('numbers/default/256'));
 			
-	    case "fivehundredtwelve":
-	        loadGraphic(Paths.image("numbers/default/512"));
+	    case 'fivehundredtwelve':
+	        loadGraphic(Paths.image('numbers/default/512'));
 			
-	    case "onethousandtwentyfour":
-	        loadGraphic(Paths.image("numbers/default/1024"));
+	    case 'onethousandtwentyfour':
+	        loadGraphic(Paths.image('numbers/default/1024'));
 	
-	    case "twothousandfourtyeight":
-	        loadGraphic(Paths.image("numbers/default/2048"));
+	    case 'twothousandfourtyeight':
+	        loadGraphic(Paths.image('numbers/default/2048'));
 			
-	    case "fourthousandninetysix":
-	        loadGraphic(Paths.image("numbers/default/4096"));
+	    case 'fourthousandninetysix':
+	        loadGraphic(Paths.image('numbers/default/4096'));
 			
-	    case "eightthousandonehundredninetytwo":
-	        loadGraphic(Paths.image("numbers/default/8192"));
+	    case 'eightthousandonehundredninetytwo':
+	        loadGraphic(Paths.image('numbers/default/8192'));
 			
-	    case "sixteenthousandthreehundredeightyfour":
-	        loadGraphic(Paths.image("numbers/default/16384"));
+	    case 'sixteenthousandthreehundredeightyfour':
+	        loadGraphic(Paths.image('numbers/default/16384'));
 	
-	    case "thirtytwothousandsevenhundredsixtyeight":
-	        loadGraphic(Paths.image("numbers/default/32768"));
+	    case 'thirtytwothousandsevenhundredsixtyeight':
+	        loadGraphic(Paths.image('numbers/default/32768'));
 			
-	    case "sixtyfivethousandfivehundredthirtysix":
-	        loadGraphic(Paths.image("numbers/default/65536"));
+	    case 'sixtyfivethousandfivehundredthirtysix':
+	        loadGraphic(Paths.image('numbers/default/65536'));
 			
-	    case "onehundredthirtyonethousandseventytwo":
-	        loadGraphic(Paths.image("numbers/default/131072"));
+	    case 'onehundredthirtyonethousandseventytwo':
+	        loadGraphic(Paths.image('numbers/default/131072'));
 			
-	    case "twohundredsixtytwothousandonehundredfourtyfour":
-	        loadGraphic(Paths.image("numbers/default/262144"));
+	    case 'twohundredsixtytwothousandonehundredfourtyfour':
+	        loadGraphic(Paths.image('numbers/default/262144'));
 	
-	    case "fivehundredtwentyfourthousandtwohundredeightyeight":
-	        loadGraphic(Paths.image("numbers/default/524288"));
+	    case 'fivehundredtwentyfourthousandtwohundredeightyeight':
+	        loadGraphic(Paths.image('numbers/default/524288'));
 			
-	    case "onemillionfourtyeighthousandfivehundredseventysix":
-	        loadGraphic(Paths.image("numbers/default/1048576"));
+	    case 'onemillionfourtyeighthousandfivehundredseventysix':
+	        loadGraphic(Paths.image('numbers/default/1048576'));
 	}
     }
 			
@@ -93,56 +92,56 @@ class Exponent extends FlxSprite
 	    
 	switch(expo)
 	{
-	    case "twentyone":
-	        loadGraphic(Paths.image("numbers/exponents/21"));
+	    case 'twentyone':
+	        loadGraphic(Paths.image('numbers/exponents/21'));
 			
-	    case "twentytwo":
-	        loadGraphic(Paths.image("numbers/exponents/22"));
+	    case 'twentytwo':
+	        loadGraphic(Paths.image('numbers/exponents/22'));
 			
-	    case "twentythree":
-	        loadGraphic(Paths.image("numbers/exponents/23"));
+	    case 'twentythree':
+	        loadGraphic(Paths.image('numbers/exponents/23'));
 	
-	    case "twentyfour":
-	        loadGraphic(Paths.image("numbers/exponents/24"));
+	    case 'twentyfour':
+	        loadGraphic(Paths.image('numbers/exponents/24'));
 			
-	    case "twentyfive":
-	        loadGraphic(Paths.image("numbers/exponents/25"));
+	    case 'twentyfive':
+	        loadGraphic(Paths.image('numbers/exponents/25'));
 			
-	    case "twentysix":
-	        loadGraphic(Paths.image("numbers/exponents/26"));
+	    case 'twentysix':
+	        loadGraphic(Paths.image('numbers/exponents/26'));
 			
-	    case "twentyseven":
-	        loadGraphic(Paths.image("numbers/exponents/27"));
+	    case 'twentyseven':
+	        loadGraphic(Paths.image('numbers/exponents/27'));
 			
-	    case "twentyeight":
-	        loadGraphic(Paths.image("numbers/exponents/28"));
+	    case 'twentyeight':
+	        loadGraphic(Paths.image('numbers/exponents/28'));
 	
-	    case "twentynine":
-	        loadGraphic(Paths.image("numbers/exponents/29"));
+	    case 'twentynine':
+	        loadGraphic(Paths.image('numbers/exponents/29'));
 			
-	    case "thirty":
-	        loadGraphic(Paths.image("numbers/exponents/30"));
+	    case 'thirty':
+	        loadGraphic(Paths.image('numbers/exponents/30'));
 			
-	    case "fourty":
-	        loadGraphic(Paths.image("numbers/exponents/40"));
+	    case 'fourty':
+	        loadGraphic(Paths.image('numbers/exponents/40'));
 			
-	    case "fifty":
-	        loadGraphic(Paths.image("numbers/exponents/50"));
+	    case 'fifty':
+	        loadGraphic(Paths.image('numbers/exponents/50'));
 			
-	    case "sixty":
-	        loadGraphic(Paths.image("numbers/exponents/60"));
+	    case 'sixty':
+	        loadGraphic(Paths.image('numbers/exponents/60'));
 			
-	    case "seventy":
-	        loadGraphic(Paths.image("numbers/exponents/70"));
+	    case 'seventy':
+	        loadGraphic(Paths.image('numbers/exponents/70'));
 			
-	    case "eighty":
-	        loadGraphic(Paths.image("numbers/exponents/80"));
+	    case 'eighty':
+	        loadGraphic(Paths.image('numbers/exponents/80'));
 			
-	    case "ninety":
-	        loadGraphic(Paths.image("numbers/exponents/90"));
+	    case 'ninety':
+	        loadGraphic(Paths.image('numbers/exponents/90'));
 			
-	    case "onehundred":
-	        loadGraphic(Paths.image("numbers/exponents/100"));
+	    case 'onehundred':
+	        loadGraphic(Paths.image('numbers/exponents/100'));
 	}
     }
 			
@@ -157,29 +156,29 @@ class Infinity extends FlxSprite
     public function new(x:Float, y:Float) {
 	super(x, y);
 
-	var tex = Paths.getSparrowAtlas("numbers/infinity");
+	var tex = Paths.getSparrowAtlas('numbers/infinity');
 	frames = tex;
 
-	animation.addByPrefix('R', "red", 1, false);
-	animation.addByPrefix('SC', "scarlet", 1, false);
-	animation.addByPrefix('O', "orange", 1, false);
-	animation.addByPrefix('YO', "yelloworange", 1, false);
-	animation.addByPrefix('Y', "yellow", 1, false);
-	animation.addByPrefix('LI', "lime", 1, false);
-	animation.addByPrefix('G', "green", 1, false);
-	animation.addByPrefix('MI', "mint", 1, false);
-	animation.addByPrefix('CY', "cyan", 1, false);
-	animation.addByPrefix('SB', "skyblue", 1, false);
-	animation.addByPrefix('B', "blue", 1, false);
-	animation.addByPrefix('RP', "royalpurple", 1, false);
-	animation.addByPrefix('P', "purple", 1, false);
-	animation.addByPrefix('WHAT', "this", 1, false);
-	animation.addByPrefix('MG', "magenta", 1, false);
-	animation.addByPrefix('HP', "hotpink", 1, false);
-	animation.addByPrefix('LOOP', "loop", 1, false);
+	animation.addByPrefix('R', 'red', 1, false);
+	animation.addByPrefix('SC', 'scarlet', 1, false);
+	animation.addByPrefix('O', 'orange', 1, false);
+	animation.addByPrefix('YO', 'yelloworange', 1, false);
+	animation.addByPrefix('Y', 'yellow', 1, false);
+	animation.addByPrefix('LI', 'lime', 1, false);
+	animation.addByPrefix('G', 'green', 1, false);
+	animation.addByPrefix('MI', 'mint', 1, false);
+	animation.addByPrefix('CY', 'cyan', 1, false);
+	animation.addByPrefix('SB', 'skyblue', 1, false);
+	animation.addByPrefix('B', 'blue', 1, false);
+	animation.addByPrefix('RP', 'royalpurple', 1, false);
+	animation.addByPrefix('P', 'purple', 1, false);
+	animation.addByPrefix('WHAT', 'this', 1, false);
+	animation.addByPrefix('MG', 'magenta', 1, false);
+	animation.addByPrefix('HP', 'hotpink', 1, false);
+	animation.addByPrefix('LOOP', 'loop', 1, false);
     }
 
-	override public function update(elapsed:Float):Void
+    override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
     }
