ca83d5e0cbbbe8f183ca65dfedb0b4fff1153244
Joalor64 <rhgarcon@gmail.com>
2023-04-10 20:00:25 -0400

    fixes for playstate lmao

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -53,130 +53,130 @@ class PlayState extends FlxState
 
         if (clicks <= 21) 
         {
-            if (clicks == 0)
+            if (clicks == 0) {
                 num = new Number(0, 0, "one");
-            else if (clicks == 1)
+            } else if (clicks == 1) {
                 remove(num);
                 num = new Number(0, 0, "two");
-            else if (clicks == 2)
+            } else if (clicks == 2) {
                 remove(num);
                 num = new Number(0, 0, "four");
-            else if (clicks == 3)
+            } else if (clicks == 3) {
                 remove(num);
                 num = new Number(0, 0, "eight");
-            else if (clicks == 4)
+            } else if (clicks == 4) {
                 remove(num);
                 num = new Number(0, 0, "sixteen");
-            else if (clicks == 5)
+            } else if (clicks == 5) {
                 remove(num);
                 num = new Number(0, 0, "thirtytwo");
-            else if (clicks == 6)
+            } else if (clicks == 6) {
                 remove(num);
                 num = new Number(0, 0, "sixtyfour");
-            else if (clicks == 7)
+            } else if (clicks == 7) {
                 remove(num);
                 num = new Number(0, 0, "onehundredtwentyeight");
-            else if (clicks == 8)
+            } else if (clicks == 8) {
                 remove(num);
                 num = new Number(0, 0, "twohundredfiftysix");
-            else if (clicks == 9)
+            } else if (clicks == 9) {
                 remove(num);
                 num = new Number(0, 0, "fivehundredtwelve");
-            else if (clicks == 10)
+            } else if (clicks == 10) {
                 remove(num);
                 num = new Number(0, 0, "onethousandtwentyfour");
-            else if (clicks == 11)
+            } else if (clicks == 11) {
                 remove(num);
                 num = new Number(0, 0, "twothousandfourtyeight");
-            else if (clicks == 12)
+            } else if (clicks == 12) {
                 remove(num);
                 num = new Number(0, 0, "fourthousandninetysix");
-            else if (clicks == 13)
+            } else if (clicks == 13) {
                 remove(num);
                 num = new Number(0, 0, "eightthousandonehundredninetytwo");
-            else if (clicks == 14)
+            } else if (clicks == 14) {
                 remove(num);
                 num = new Number(0, 0, "sixteenthousandthreehundredeightyfour");
-            else if (clicks == 15)
+            } else if (clicks == 15) {
                 remove(num);
                 num = new Number(0, 0, "thirtytwothousandsevenhundredsixtyeight");
-            else if (clicks == 16)
+            } else if (clicks == 16) {
                 remove(num);
                 num = new Number(0, 0, "sixtyfivethousandfivehundredthirtysix");
-            else if (clicks == 17)
+            } else if (clicks == 17) {
                 remove(num);
                 num = new Number(0, 0, "onehundredthirtyonethousandseventytwo");
-            else if (clicks == 18)
+            } else if (clicks == 18) {
                 remove(num);
                 num = new Number(0, 0, "twohundredsixtytwothousandonehundredfourtyfour");
-            else if (clicks == 19)
+            } else if (clicks == 19) {
                 remove(num);
                 num = new Number(0, 0, "fivehundredtwentyfourthousandtwohundredeightyeight");
-            else if (clicks == 20)
+            } else if (clicks == 20) {
                 remove(num);
                 num = new Number(0, 0, "onemillionfourtyeighthousandfivehundredseventysix");
+            }
             num.screenCenter();
             add(num);
         } 
-        else if (clicks >= 21 && clicks <= 37) 
-        {
-            if (clicks == 21)
+        else if (clicks >= 21 && clicks <= 37) {
+            if (clicks == 21) {
                 remove(num);
                 expo = new Exponent(0, 0, "twentyone");
-            else if (clicks == 22)
+            } else if (clicks == 22) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentytwo");
-            else if (clicks == 23)
+            } else if (clicks == 23) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentythree");
-            else if (clicks == 24)
+            } else if (clicks == 24) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentyfour");
-            else if (clicks == 25)
+            } else if (clicks == 25) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentyfive");
-            else if (clicks == 26)
+            } else if (clicks == 26) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentysix");
-            else if (clicks == 27)
+            } else if (clicks == 27) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentyseven");
-            else if (clicks == 28)
+            } else if (clicks == 28) {
                 remove(expo);
                 expo = new Exponent(0, 0, "twentyeight");
-            else if (clicks == 29)
+            } else if (clicks == 29) {
                 remove(expo)
                 expo = new Exponent(0, 0, "twentynine");
-            else if (clicks == 30)
+            } else if (clicks == 30) {
                 remove(expo);
                 expo = new Exponent(0, 0, "thirty");
-            else if (clicks == 31)
+            } else if (clicks == 31) {
                 remove(expo);
                 expo = new Exponent(0, 0, "fourty");
-            else if (clicks == 32)
+            } else if (clicks == 32) {
                 remove(expo);
                 expo = new Exponent(0, 0, "fifty");
-            else if (clicks == 33)
+            } else if (clicks == 33) {
                 remove(expo);
                 expo = new Exponent(0, 0, "sixty");
-            else if (clicks == 34)
+            } else if (clicks == 34) {
                 remove(expo);
                 expo = new Exponent(0, 0, "seventy");
-            else if (clicks == 35)
+            } else if (clicks == 35){
                 remove(expo);
                 expo = new Exponent(0, 0, "eighty");
-            else if (clicks == 36)
+            } else if (clicks == 36) {
                 remove(expo);
                 expo = new Exponent(0, 0, "ninety");
-            else if (clicks == 37)
+            } else if (clicks == 37) {
                 remove(expo);
                 expo = new Exponent(0, 0, "onehundred");
+            }
             expo.screenCenter();
             add(expo);
             remove(num);
         } 
-        else if (clicks >= 38) 
-        {
+        else if (clicks >= 38) {
             remove(expo);
             infinity = new Infinity(0, 0);
             infinity.screenCenter();
